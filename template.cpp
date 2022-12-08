2a3b2ccf16da367d02cc8c03393485c8be686129
justinzhang <justinz@andrew.cmu.edu>
2022-12-08 13:29:37 -0500

    template added

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+int main() {
+    ifstream infile("input.txt");
+
+
+    for (string line; getline(infile,line);) {
+    }
+    return 0;
+}
